43048bb88882e72be1dbf8b5b2595da01b375b00
YuLong <1003716039@qq.com>
2022-05-26 23:33:39 +0800

    显示评论（后端逻辑还ok，前端页面处理有点麻烦）仔细、认真。

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -26,4 +26,14 @@ public interface CommunityConstant {
      * 记住状态登录凭证的过期时间，100天
      */
     int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
+
+    /**
+     * 实体类型：帖子
+     */
+    int ENTITY_TYPE_DISCUSS_POST = 1;
+
+    /**
+     * 实体类型：评论
+     */
+    int ENTITY_TYPE_COMMENT = 2;
 }
