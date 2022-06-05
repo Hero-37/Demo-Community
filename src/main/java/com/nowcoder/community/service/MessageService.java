95e70ff63fed7c1e64b2dd79d8cb1b3a52fd3e63
YuLong <1003716039@qq.com>
2022-06-06 01:26:58 +0800

    发送私信

--- src/main/java/com/nowcoder/community/service/MessageService.java
+++ src/main/java/com/nowcoder/community/service/MessageService.java
@@ -2,8 +2,11 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.MessageMapper;
 import com.nowcoder.community.entity.Message;
+import com.nowcoder.community.util.SensitiveFilter;
+import org.attoparser.dom.INestableNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -12,6 +15,9 @@ public class MessageService {
 
     @Autowired
     private MessageMapper messageMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
 
     public List<Message> findConversations(int userId, int offset, int limit) {
         return messageMapper.selectConversations(userId, offset, limit);
@@ -31,5 +37,20 @@ public class MessageService {
 
     public int findLetterUnreadCount(int userId, String conversationId) {
         return messageMapper.selectLetterUnreadCount(userId, conversationId);
+    }
+
+    public int addMessage(Message message) {
+        message.setContent(HtmlUtils.htmlEscape(message.getContent()));
+        message.setContent(sensitiveFilter.filter(message.getContent()));
+        return messageMapper.insertMessage(message);
+    }
+
+    /**
+     * 改变消息状态，将消息改为已读
+     * @param ids
+     * @return
+     */
+    public int readMessage(List<Integer> ids) {
+        return messageMapper.updateStatus(ids, 1);
     }
 }
--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -272,4 +272,8 @@ public class UserService implements CommunityConstant {
         userMapper.updatePassword(userId, newPassword);
         return map;
     }
+
+    public User findUserByName(String username) {
+        return userMapper.selectByName(username);
+    }
 }
