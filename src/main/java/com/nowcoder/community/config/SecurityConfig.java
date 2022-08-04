f354f40afd65b75bcd419694294aa9f1446b0291
YuLong <1003716039@qq.com>
2022-08-04 11:59:38 +0800

    引入 SpringSecurity 的权限控制

---
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -0,0 +1,89 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import javafx.print.Printer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements CommunityConstant {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 授权
+        http.authorizeRequests()
+                .antMatchers(
+                        "/user/setting",
+                        "/user/upload",
+                        "/discuss/add",
+                        "/comment/add/**",
+                        "/letter/**",
+                        "/notice/**",
+                        "/like",
+                        "/follow",
+                        "/unfollow"
+
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_USER,
+                        AUTHORITY_ADMIN,
+                        AUTHORITY_MODERATOR
+                )
+                .anyRequest().permitAll()
+                .and().csrf().disable();  // 不启用 csrf，若要启用，须自己配置每个页面，生成 csrf 凭证，详见 index.html
+
+        // 权限不够时的处理
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    // 没有登录
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        String xRequestedWith = request.getHeader("x-requested-with");
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {  // 异步请求
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter printWriter = response.getWriter();
+                            printWriter.write(CommunityUtil.getJsonString(403, "你还没有登录哦！"));
+                        } else {  // 同步请求
+                            response.sendRedirect(request.getContextPath() + "/login");
+                        }
+                    }
+                })
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    // 权限不足
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        String xRequestedWith = request.getHeader("x-requested-with");
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {  // 异步请求
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter printWriter = response.getWriter();
+                            printWriter.write(CommunityUtil.getJsonString(403, "你没有访问此功能的权限!"));
+                        } else {  // 同步请求
+                            response.sendRedirect(request.getContextPath() + "/denied");
+                        }
+                    }
+                });
+
+        // Security 底层会默认拦截 /logout 请求，进行退出处理
+        // 覆盖它默认的逻辑，才能执行我们自己的退出代码
+        http.logout().logoutUrl("/securityLogout");
+    }
+}
